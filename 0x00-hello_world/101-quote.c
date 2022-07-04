743b80fadd7c8adca6bb1c9d338953b3df9c295d
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-04 06:10:33 -0700

    Write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Writes a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error."
+ * Return: 1
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (0);
+}
