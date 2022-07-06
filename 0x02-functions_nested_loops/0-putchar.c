689a4f3f57d0f6d4edeb3ef3e9c7d6c54a437ce4
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-05 19:36:01 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a _putchar, followed by a new line.
+ * Return: 0 if exited properly, non-zero number otherwise.
+ */
+
+int main(void)
+{
+	printf("_putchar");
+}
