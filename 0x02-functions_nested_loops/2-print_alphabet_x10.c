a6e6051b4359dcb9ce405b1616fa1c279e93b1f9
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-06 13:19:45 -0700

    Alpha x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 prints 10 the alphabet 10 times,
+ * print_alphabet_x10 - prints 10 the alphabet 10 times,
  * in lowercase, followed by a new line.
  * Return: 0 if exited properly, otherwise non-zero number.
  */
@@ -16,7 +16,6 @@ void print_alphabet_x10(void)
 		{
 			_putchar(j);
 		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
