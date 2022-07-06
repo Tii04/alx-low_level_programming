dab9c9b4a502f591fdffdb736f55197421c9ad34
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-06 13:26:53 -0700

    Alpha 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - prints 10 the alphabet 10 times,
  * in lowercase, followed by a new line.
- * Return: 0 if exited properly, otherwise non-zero number.
+ * Return: void.
  */
 void print_alphabet_x10(void)
 {
