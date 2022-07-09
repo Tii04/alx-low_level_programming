c370c3301405361079bed58e3a250c04004393f4
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-09 07:59:54 -0700

    Print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,7 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, r;
 
 	if (n <= 0)
 	{
@@ -16,9 +16,12 @@ void print_diagonal(int n)
 	else
 	{
 		for (i = 0; i < n; i++)
-			_putchar(92);
-
-		_putchar('\n');
+		{
+			for (r = 0; r < i; r++)
+			{	_putchar(' ');
+				_putchar(92);
+				_putchar('\n');
+			}
+		}
 	}
-		return;
 }
