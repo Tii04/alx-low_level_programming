956599457f37ae0ac7ff94041c442f9aa9c9d336
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-09 12:55:37 -0700

    Print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line.
+ * @size: integer of type size as argument.
+ * Return: void.
+ */
+void print_square(int size)
+{
+	int i, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (y = 0; y < i; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,3 +6,4 @@ Task 4. A function that prints the numbers, from 0 to 9, followed by a new line.
 Task 5. A function that prints 10 times the numbers, from 0 to 14, followed by a new line.
 Task 6. A function that draws a straight line in the terminal.
 Task 7. A function that draws a diagonal line on the terminal.
+Task 8. A function that prints a square, followed by a new line.
