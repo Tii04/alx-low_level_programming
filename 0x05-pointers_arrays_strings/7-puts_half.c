f6a4a73807528af2f95e151fe6c8396212ed9eec
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-11 20:52:43 -0700

    Half-half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * puts_half - prints half of a string,
@@ -8,5 +9,20 @@
  */
 void puts_half(char *str)
 {
-	
+	int x, y, i;
+
+	x = strlen(str);
+	if (x % 2 == 1)
+	{
+		y = x / 2 + 1;
+	}
+	else
+	{
+		y = x / 2;
+	}
+	for (i = y; i < x; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
