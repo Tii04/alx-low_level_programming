b8621ea18cd9a1f9c50a2910a69675df800b2ffa
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-11 17:57:01 -0700

    Lengthy

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: char pointer variable
+ * Return: int.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	while (*s != '\0')
+	{
+		length++;
+		*s = *s + 1;
+	}
+	return (length);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 Task 0. A function that takes a pointer to an int as parameter and updates the value it points to 98.
 Task 1. A function that swaps the values of two integers.
+Task 2. A function that returns the length of a string.
