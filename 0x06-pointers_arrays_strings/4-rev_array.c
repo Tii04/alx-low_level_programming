dae2fe42853173769d7b3588358d32d0b64e6dc5
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-14 20:19:38 -0700

    Reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,19 +8,18 @@
  */
 void reverse_array(int *a, int n)
 {
-	int a = [1,2,3,4,5,6];
+	int *p, i, aux, k;
 	
-	int tmp, i;
+	p = a;
+
+	for (i = 0; i < n; i++)
+		p++
 
 	for (i = 0; i < n / 2; i++)
 	{
-		tmp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = tmp;
-	}
-	
-	for (int i = 0; i < n; i++)
-	{
-		_putchar(a[i]);
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
 	}
 }
