aa608d6b60c9949284bc733c3e5f058031897b30
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-15 20:07:45 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes
+ * a string into 1337.
+ * @x: parameter x.
+ * Return: string.
+ */
+
+char *leet(char *x)
+{
+	int a = 0, b, l = 5;
+
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+
+	char trs[5] = {'4', '3', '0', '7', '1'};
+
+	while (x[a])
+	{
+		b = 0;
+
+		while (b < l)
+		{
+			if (x[a] == tr[b] || x[a] - 32 == tr[b])
+			{
+				x[a] = trs[b];
+			}
+			b++;
+		}
+		a++;
+		
+	}
+	return (x);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -4,4 +4,5 @@ Task 2. A function that copies a string.
 Task 3. A function that compares two strings.
 Task 4. A function that reverses an array.
 Task 5. A function that changes all lowercase letters of a string to uppercase.
-Task 6. A function that capitalizes all words of a string..
+Task 6. A function that capitalizes all words of a string.
+Task 7. A function that encodes a string into 1337.
