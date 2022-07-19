13185657714d858a7370f9efdb70ef12bdcfc3b2
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-18 19:09:18 -0700

    Memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - fills memory with
+ * a constant byte.
+ * @s: character parameter.
+ * @b: character parameter.
+ * @n: unsigned parameter.
+ * Return: pointer.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, 10);
+
+	return (s);
+}
