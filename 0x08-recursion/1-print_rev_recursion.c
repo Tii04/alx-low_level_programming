28ec92756104951773da847d351c305bf886fad7
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-22 10:21:27 -0700

    Reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion -
- * Prints a string in reverse.
+ * _print_rev_recursion - prints
+ * a string in reverse.
  * @s: character param.
  * Return: string.
  */
