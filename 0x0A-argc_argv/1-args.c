7eec05c9a8fba1df4ee1fcc6b753253edea05ed4
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-26 12:21:46 -0700

    Arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of
+ * arguments.
+ * @argc: counter.
+ * @argv: vector.
+ * Return: int.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
