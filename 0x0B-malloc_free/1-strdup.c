9a163ea8562284dd951df3509df3319fb9ccec5f
Tiisetso <tiisetso.qsebata@gmail.com>
2022-07-31 18:51:55 -0700

    New pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strdup - returns pointer to new string.
+ * @str: pointer char.
+ * Return: pointer.
+ */
+
+char *_strdup(char *str)
+{
+	int i = 1, j = 0;
+
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+		i++;
+
+	s = (char*)malloc(i * sizeof(char) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+
+	s[j] = '\0';
+	return (s);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,8 @@
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
 #endif
