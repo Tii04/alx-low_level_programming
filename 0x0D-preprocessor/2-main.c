acce85fa5d34660663752615e8bcc5c7c1237f2a
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-02 19:39:41 -0700

    Where are you from??

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - prints where file
+ * was compiled from.
+ * Return: 0 on success, non-zero otherwise.
+ */
+
+int main(void)
+{
+	_putcha(__FILE__);
+
+	return (0);
+}
