bc4c97140c63ebf50e213f35ecfa44ec7c5ba84d
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-06 11:40:13 -0700

    Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,34 +2,16 @@
 #include "function_pointers.h"
 
 /**
- * print - prints name.
- * @name: name to print.
- * Return: Void.
- */
-
-void print(char *name)
-{
-	  printf("Hey %s\n", name);
-}
-
-/**
- * print_name - uses print() to print name.
+ * print_name - function to print name.
  * @name: name to be printed.
  * @f: function pointer.
+ * Return: void.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f = &print;
-	    f(name);
-}
-
-/**
- * main - executes.
- * Return: void.
- */
-
-int main(void)
-{
-	  print_name("Tiisetso", print);
+	if (name && f)
+	{
+		f(name);
+	}
 }
