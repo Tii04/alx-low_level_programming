ca8409290488b857e9e98037f16f422368b23754
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-12 20:04:28 -0700

    Printing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,10 +16,10 @@ size_t print_list(const list_t *h)
 		count++;
 		if (h->str == NULL)
 		{
-			printf("[%d] %s", 0, "(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
-			printf("[%d] %s", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
 	}
