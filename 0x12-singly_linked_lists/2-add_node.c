12f7c6aa52eabd4ff086e3dc94e73651f68bdcda
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-12 20:53:07 -0700

    Adding node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t new_node;
+	list_t *new_node;
 	size_t i;
 
 	new_node = malloc(sizeof(list_t));
@@ -23,7 +23,7 @@ list_t *add_node(list_t **head, const char *str)
 	for (i = 0; str[i]; i++)
 		;
 
-	new_node->len = n;
+	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 
