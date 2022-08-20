c5c3c87c8e886ea1f523fa8f95365e42384046cd
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-20 11:14:07 -0700

    Converter

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,8 +8,9 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total = 0, decval = 1, i;
+	unsigned int total = 0, decval = 1;
 	unsigned int slen = strlen(b);
+	int i;
 
 	if (b == NULL)
 		return (0);
