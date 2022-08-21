5a0f563fbc2205e507ee920e8f890958c48ebad4
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-21 10:03:37 -0700

    1 converter

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * set_bit - sets bit to 1 at given index.
+ * @n: decimal param.
+ * @index: index.
+ * Return: 1 or -1 if unsuccessful.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+
+	*n | = (1 << index);
+	return (1);
+}
