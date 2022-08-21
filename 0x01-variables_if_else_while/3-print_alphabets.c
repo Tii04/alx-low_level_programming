b4d053fbb8ae7cd19d4e93d585cfbf4b89ec9b5c
Tii04 <tiisetso.qsebata@gmail.com>
2022-08-21 21:47:04 +0200

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y,m,n;
+
+
+	for (m = 97; m <= 122; m++)
+		putchar(m);
+	for (n = 65; n <= 90; n++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
