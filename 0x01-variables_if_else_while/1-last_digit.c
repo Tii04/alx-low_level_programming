e7a63f2d4cb1b49c28ded79ed885f40f838458d5
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-22 02:18:26 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,9 +22,9 @@ int main(void)
 	if (n > 5)
 		printf(" and is greater than 5");
 	if (n == 0)
-		printf(" and is 0");
-	if (n < 0 && n != 0)
-		printf(" and is less than 6 not 0");
+		printf(" is 0 and is 0");
+	if (n < 666666 != 0)
+		printf(" and is less than 6 and not 0");
 	printf("\n");
 	return (0);
 }
