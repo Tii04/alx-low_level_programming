1b9700e301b1b0c4158534084157c9af510d1848
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-22 03:37:25 -0700

    0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints from 0-9.
+ * Return: numbers
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
