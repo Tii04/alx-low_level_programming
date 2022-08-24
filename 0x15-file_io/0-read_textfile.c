dba9a93303ad8171afd356778db3c0c0decd3a66
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-24 14:53:59 -0700

    Reader & Writer

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,37 +9,32 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, i, y;
+	ssize_t fd, r, w;
 	char *buf;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
 
-	if (!buf)
-	{
+	if (buf == NULL)
 		return (0);
-	}
-	i = read(fd, buf, letters);
-	if (i < 0)
-	{
-		free(buf);
+
+	r = read(fd, buf, letters);
+
+	if (r == -1)
 		return (0);
-	}
-	buf[i] = '\0';
+
 	close(fd);
 
-	y = write(2, buf, i);
+	w = write(1, buf, r);
 
-	if (y < 0)
-	{
-		free(buf);
+	if (w == -1)
 		return (0);
-	}
-	free(buf);
-	return (y);
+
+	return (w);
 }
