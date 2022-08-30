a7b933868646fd5f49826fe63b9b999b6b81d60c
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-29 23:53:19 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value.
+ * @n: number to be converted.
+ * Return: absolute number.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (0);
+}
