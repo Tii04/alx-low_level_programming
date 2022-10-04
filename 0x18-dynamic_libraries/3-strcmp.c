24fcdee4747cafbf6deefaa2108e24a86698d989
Tii04 <Tii04@github.com>
2022-10-04 13:02:57 +0200

    dynlib

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: String 1.
+ * @s2: String 2.
+ * Return: string.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+
+	return (result);
+
+}
