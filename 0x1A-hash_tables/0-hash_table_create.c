0148e4c7b5b0b532f6c212e9985ce9d7ef6a642e
Tii04 <Tii04@github.com>
2022-10-23 19:40:08 +0200

    djb2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,9 @@
 #include "hash_tables.h"
 
 /*
- * hash_table_create- creates hash table.
- * @size: Size of array.
- * Return: pointer to hash table, otherwise
- * NULL
+ * hash_table_create - Creates a hash table.
+ * @size: Specified size of array
+ * Return: pointer to hash table. If error, NULL.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
